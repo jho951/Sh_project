f1fab0ef1834c50c578d7669efb6655a9aafd05f
jho951 <jho951@naver.com>
2024-09-30 10:58:09 +0900

    [COMMIT]테스

--- Jh_test/src/test/test1.java
+++ Jh_test/src/test/test1.java
@@ -2,7 +2,8 @@ package test;
 
 public class test1 {
 	public static void main(String[] args) {
-		int example=200;
-		System.out.println("예시 : " + example );
+		int example1 = 200;
+		int example2 = 100;
+		System.out.println("예시 : " + example1 + example2 );
 	}
 }
