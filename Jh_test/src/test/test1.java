d0149efb7bd2a93b747866fa674db35f5cb5aafa
jho951 <jho951@naver.com>
2024-09-30 11:35:06 +0900

    [FEAT] ADD EXAMPLE

--- Jh_test/src/test/test1.java
+++ Jh_test/src/test/test1.java
@@ -5,8 +5,10 @@ public class test1 {
 		int example1 = 200;
 		int example3 = 300;
 		int example2 = 100; 
-		char  example4 ='는';
+		char example4 ='는';
+		int example5 = 0b101;
 	
 		System.out.println("예시"+ example4 +":" + (example1 + example2 + example3) );
+		System.out.println("이것은 이진법 형태입니다."+ example5);
 	}
 }
