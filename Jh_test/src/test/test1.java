35964a78e86cc287bdb7ad87c60ed83750b0160f
jho951 <jho951@naver.com>
2024-09-30 11:04:36 +0900

    [COMMIT] GIT 테스

--- Jh_test/src/test/test1.java
+++ Jh_test/src/test/test1.java
@@ -4,7 +4,9 @@ public class test1 {
 	public static void main(String[] args) {
 		int example1 = 200;
 		int example3 = 300;
-		int example2 = 100;
-		System.out.println("예시 : " + example1 + example2 );
+		int example2 = 100; 
+		String example4 ="는";
+	
+		System.out.println("예시"+example4 +":" + example1 + example2 + example3 );
 	}
 }
