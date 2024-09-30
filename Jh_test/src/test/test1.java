4ec1c1ebc5e34d4c446ec805ffe86736dc0ce5eb
jho951 <jho951@naver.com>
2024-09-30 11:01:31 +0900

    [COMMIT] 이클립스 깃헙 연동 테스

--- Jh_test/src/test/test1.java
+++ Jh_test/src/test/test1.java
@@ -3,6 +3,7 @@ package test;
 public class test1 {
 	public static void main(String[] args) {
 		int example1 = 200;
+		int example3 = 300;
 		int example2 = 100;
 		System.out.println("예시 : " + example1 + example2 );
 	}
